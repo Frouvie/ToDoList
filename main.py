07d9355d088a56a718d03ff1b974aac7dfca6337
Frouvie <140159514+Frouvie@users.noreply.github.com>
2025-02-07 12:53:24 +0300

    add console app

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import sqlite3
 
 conn = sqlite3.connect('my_db.db')
@@ -56,15 +57,76 @@ def select_all() -> list:
     return cursor.fetchall()
 
 
+def menu() -> None:
+    print('Menu:')
+    print('1. Select all')
+    print('2. Select')
+    print('3. Insert')
+    print('4. Update')
+    print('5. Delete')
+    print('6. Exit')
+
+
 def main():
-    create_table()
-    insert("Do push-ups", 100)
     try:
-        temp = select(id=1)
-        print(temp[1])
-    except:
-        print("Error")
-    conn.close()
+        create_table()
+    except Exception as error:
+        print(f'Error : {error}')
+        sys.exit()
+
+    menu()
+    
+    while True:
+        responce = int(input('Choice an action (1-6): '))
+
+        match responce:
+            case 1:
+                try:
+                    tasks = select_all()
+                except Exception as error:
+                    print(f'Error: {error}')
+                    break
+
+                for task in tasks:
+                    print(task)
+            case 2:
+                id = int(input('Enter id: '))
+                
+                try:
+                    task = select(id)
+                    print(task)
+                except Exception as error:
+                    print(f'Error: {error}')
+                    break
+            case 3:
+                title = input("Enter title: ")
+                priority = int(input("Enter priority: "))
+
+                try:
+                    task = insert(title, priority)
+                except Exception as error:
+                    print(f'Error: {error}')
+                    break
+            case 4:
+                id = int(input('Enter id: '))
+                title = input("Enter title: ")
+                priority = int(input("Enter priority: "))
+
+                try:
+                    task = update(id, title, priority)
+                except Exception as error:
+                    print(f'Error: {error}')
+                    break
+            case 5:
+                id = int(input('Enter id: '))
+
+                try:
+                    task = delete(id)
+                except Exception as error:
+                    print(f'Error: {error}')
+                    break
+            case 6:
+                sys.exit()
 
 
 if __name__ == '__main__':
